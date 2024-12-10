c7b6ce2e3c9dedf549f76ba7f225408add27851c
lucas <lucasturschwell@hotmail.com>
2024-12-10 11:26:29 +0100

    enemy shoot and 3 points health for player, AI for the random shooting to do

---
+++ Assets/Scenes/EnemyShootBullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyShootBullet : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 5f); // Détruit la balle au bout de 5 secondes
+    }
+
+    void Update()
+    {
+        // Déplace la balle vers le bas
+        Vector3 newPosition = transform.position;
+        newPosition.y -= 3 * Time.deltaTime;
+        transform.position = newPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Réduit les points de vie du joueur
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1); // Inflige 1 point de dégâts
+            }
+
+            Destroy(gameObject); // Détruit la balle après collision
+        }
+    }
+}
---
+++ Assets/Scenes/EnnemyShoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            // Create bullet at current position
+            GameObject bullet = Instantiate(bulletPrefab, transform.position + Vector3.down * 0.5f, Quaternion.identity);
+            
+           
+        }
+    }
+}
