38b4351dae7b31eae09fa3b53a3f1c06da3554a6
lucas <lucasturschwell@hotmail.com>
2024-12-10 16:55:18 +0100

    Fix group move

--- Assets/Scenes/DestroyOnCollision.cs
+++ Assets/Scenes/DestroyOnCollision.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
+    public string tag = "Enemy";
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Finish"))
+        if (collision.gameObject.CompareTag(tag))
         {
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
