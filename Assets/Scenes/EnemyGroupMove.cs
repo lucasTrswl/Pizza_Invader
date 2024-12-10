d9742d3f48803bb39d23701005fdc8e31f75ed33
jathurshandev <jathurshandev@gmail.com>
2024-12-10 15:47:32 +0100

    enemy movement

--- Assets/Scenes/EnemyGroupMove.cs
+++ Assets/Scenes/EnemyGroupMove.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 
 namespace Assets.Scenes
 {
-    public class EnemyIntelligence: MonoBehaviour
+    public class EnemyGroupMove: MonoBehaviour
     {
         public float speed;
         private int moveDirection;
+        public float BorderWidth = 10;
+
         // Use this for initialization
         void Start()
         {
-            speed = 5;
             moveDirection = Random.Range(0, 1) == 1 ? -1 : 1;
         }
 
@@ -19,7 +20,7 @@ namespace Assets.Scenes
         {
             Vector2 movement = new Vector2(moveDirection, 0) * speed * Time.deltaTime;
 
-            if ((transform.position.x < -10 && movement.x < 0) || (transform.position.x > 10 && movement.x > 0))
+            if ((transform.position.x < -BorderWidth && movement.x < 0) || (transform.position.x > BorderWidth && movement.x > 0))
             {
                 moveDirection *= -1;
                 return;
