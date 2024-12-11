4d4023e10b4850fba881e8f2c3c70133e2a6d3d6
lucas <lucasturschwell@hotmail.com>
2024-12-11 15:01:46 +0100

    life points in progress

--- Assets/Scenes/Common/DestroyOnCollision.cs
+++ Assets/Scenes/Common/DestroyOnCollision.cs
@@ -5,13 +5,26 @@ public class DestroyOnCollision : MonoBehaviour
     public string tag = "Enemy";
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(this.name + " " + collision.name);
         if (collision.gameObject.CompareTag(tag))
         {
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
         }
+            // Réduit les points de vie de l'ennemi
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(1); // Inflige 1 point de dégâts
 
+                // Si l'ennemi est mort, le détruire
+                if (enemyHealth.health <= 0)
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
 
+            // Détruit l'objet actuel (par exemple, le projectile)
+            Destroy(this.gameObject);
+        }
     }
-}
+
